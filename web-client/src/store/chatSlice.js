4ee41475ce567deb41d35c07de019af9e61855a6
junyang.lin <sheep6411@gmail.com>
2025-06-13 17:04:16 +0800

    feat(web-client): clear Redux state on logout for clean user session

--- web-client/src/store/chatSlice.js
+++ web-client/src/store/chatSlice.js
@@ -1,6 +1,8 @@
 import { createSlice } from '@reduxjs/toolkit';
+import { userLogout } from './authSlice';
 
 const initialState = {
+  selectedUserId: null,
   draftMessage: '',
 };
 
@@ -8,15 +10,25 @@ const chatSlice = createSlice({
   name: 'messenger',
   initialState,
   reducers: {
+    setSelectedUserId: (state, action) => {
+      state.selectedUserId = action.payload;
+    },
     setDraftMessage: (state, action) => {
       state.draftMessage = action.payload;
     }
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(userLogout.fulfilled, (state, action) => {
+          return initialState;
+        })
   }
 });
 
 // ----------- 匯出 -----------
 
 export const {
+  setSelectedUserId,
   setDraftMessage
 } = chatSlice.actions;
 
